809322e2a9c036f06107a615a281b9829925b2a7
Chicloon@gmail.com <gfhjkm1982>
2017-12-25 23:15:29 +0900

    auther submeny style done

--- client/components/MessagesList/MessageAuthor.js
+++ client/components/MessagesList/MessageAuthor.js
@@ -5,46 +5,66 @@ import styled from 'styled-components';
 const AuthorMenuWrapper = styled.div`
   position: absolute;
   z-index: 1;
-  background-color: red;
+  min-width: 33%;
+  background: #ffffff;
+  border: 1px solid #b5c3d0;
+  color: #42749b;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  & > * {
+    cursor: pointer;
+    width: 100%;
+    margin: 0;
+    padding: 2px;
+    text-align: center;
+    &: hover {
+      background: #f2f6fa;
+    }
+  }
 `;
 
-const AuthorMenu = props => <AuthorMenuWrapper> asdfasd </AuthorMenuWrapper>;
+const AuthorMenu = props => (
+  <AuthorMenuWrapper>
+    <p
+      key="1st"
+      onClick={() => {
+        console.log('clicked menu item');
+      }}
+    >
+      asdfasdf
+    </p>
+    <p key="2nd"> asdfasdf </p>
+    <p key="3rd"> asdfasdf </p>
+  </AuthorMenuWrapper>
+);
 
 class MessageAuthor extends React.Component {
   state = {
     showMenu: false,
   };
 
-  menuTrigger = () => {
-    this.setState(state => ({ showMenu: !state.showMenu }));
-    console.log(this.state.showMenu);
-  };
-
   render() {
-    const { username, id } = this.props;
+    const { username, id, created_at } = this.props;
     const { showMenu } = this.state;
     return (
-      <div>
-        <a
-          // onMouseEnter={this.menuTrigger}
-          // onMouseLeave={this.setState({ showMenu: false })}
-          onClick={this.menuTrigger}
-          style={{ cursor: 'pointer' }}
-        >
-          {username}
-        </a>
+      // eslint-disable-next-line
+      <div
+        style={{ cursor: 'pointer' }}
+        onClick={() => {
+          console.log('click');
+          this.setState({ showMenu: true });
+        }}
+        onMouseLeave={() => {
+          console.log('out');
+          this.setState({ showMenu: false });
+        }}
+      >
+        {username}
         {showMenu && <AuthorMenu />}
       </div>
     );
-    // return (
-    //   <Popup
-    //     trigger={<a style={{ cursor: 'pointer' }}>{username}</a>}
-    //     hideOnScroll
-    //     on="click"
-    //     position="bottom left"
-    //     content="Add users to your feed"
-    //   />
-    // );
   }
 }
 
